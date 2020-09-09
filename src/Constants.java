2d7589b63fed2d9fa7d114bae5cd9f268861d0b5
avia <avia@DESKTOP-T2T06F1>
2020-09-09 15:09:41 +0300

    Changes in chrome driver path

--- src/Constants.java
+++ src/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
     public static final Long FIVE_SEC = 5000L;
 
     public static final String YAD2_URL = "https://my.yad2.co.il/login.php";
-    public static final String DRIVER_PATH = "C:\\Users\\avia\\Desktop\\SeleniumTests\\src\\chromedriver.exe";
+    public static final String DRIVER_PATH = "C:\\Users\\avia\\Desktop\\SeleniumTests\\Yad2\\src\\chromedriver.exe";
     public static final String DRIVER_NAME = "webdriver.chrome.driver";
 
 }
